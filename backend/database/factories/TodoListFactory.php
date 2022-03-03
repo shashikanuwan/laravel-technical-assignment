3e7e2dbea7be038fcb3d190b8aa008847a7d6a5f
ShashikaNuwan <kumararanaweera1999@gmail.com>
2022-03-04 00:12:48 +0530

    write feature test

--- backend/database/factories/TodoListFactory.php
+++ backend/database/factories/TodoListFactory.php
@@ -11,7 +11,7 @@ class TodoListFactory extends Factory
     {
         return [
             'name' => $this->faker->sentence(),
-            'user_id' => User::query()->inRandomOrder()->first()->id,
+            'user_id' => User::factory(), //User::query()->inRandomOrder()->first()->id,
         ];
     }
 }
---
+++ backend/tests/Unit/TodoListTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_a_todo_list_can_has_many_tasks()
+    {
+        $list = $this->createTodoList();
+        $this->createTask(['todo_list_id' => $list->id]);
+
+        $this->assertInstanceOf(Collection::class, $list->tasks);
+        $this->assertInstanceOf(Task::class, $list->tasks->first());
+    }
+
+    public function test_todo_list_belongs_ot_a_has_user()
+    {
+        $user = $this->createUser();
+        $list = $this->createTodoList(['user_id' => $user->id]);
+
+        $this->assertInstanceOf(User::class, $list->user);
+    }
+
+    public function test_if_todo_list_is_deleted_then_all_its_tasks_will_be_deleted()
+    {
+        $list = $this->createTodoList();
+        $task = $this->createTask(['todo_list_id' => $list->id]);
+        $task2 = $this->createTask();
+
+        $list->delete();
+
+        $this->assertDatabaseMissing('todo_lists', ['id' => $list->id]);
+        $this->assertDatabaseMissing('tasks', ['id' => $task->id]);
+        $this->assertDatabaseHas('tasks', ['id' => $task2->id]);
+    }
+}
